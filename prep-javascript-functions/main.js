28b64961f65168833c388d2def723480436a79ea
Barbara Liao <barbara.liao3@gmail.com>
2021-08-10 20:41:25 -0700

    Completed hours to minutes function.

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,13 @@
+function addTwoNumbers(x, y) {
+  return x + y;
+}
+
+var sum = addTwoNumbers(4, 4);
+console.log('sum', sum);
+
+function convertHoursToMinutes(x) {
+  return x * 60;
+}
+
+var multiply = convertHoursToMinutes(3);
+console.log('multiply', multiply);
